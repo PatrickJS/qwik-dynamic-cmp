a8b5632bd5df8b4e087d042ceb710afd86a5248f
PatrickJS <github@gdi2290.com>
2023-04-16 12:57:00 -0700

    refactor: wip remote

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+// import { importMaps } from 'vite-plugin-import-maps';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity({
+
+      }),
+      qwikVite({
+      }),
+      tsconfigPaths(),
+      // importMaps([
+      //   {
+      //     imports: {
+      //       "AppComponents/": "https://qwikstarter6gcwhe-c00j-e1q6kcqa--5173--c8358679.local-credentialless.webcontainer.io/src/components/"
+      //     },
+      //   },
+      // ]),
+    ],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
